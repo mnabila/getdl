b13e714702a4f88026e95208c8e1265e2df02648
M. Nabil Adani <nblid48@gmail.com>
2023-01-09 09:07:27 +0700

    refactor: update scrape test

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/gocolly/colly/v2 v2.1.0
 	github.com/spf13/cobra v1.3.0
+	github.com/stretchr/testify v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
@@ -14,11 +15,13 @@ require (
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.3.1 // indirect
 	github.com/antchfx/xpath v1.1.10 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
--- internal/scrape/scrape_test.go
+++ internal/scrape/scrape_test.go
@@ -6,6 +6,8 @@ import (
 	"getdl/internal/scrape"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func writeToJson(data scrape.ScrapeResponse, website string) error {
@@ -17,27 +19,46 @@ func writeToJson(data scrape.ScrapeResponse, website string) error {
 	return os.WriteFile(fmt.Sprintf("result-%s.json", website), result, 0666)
 }
 
-func TestDoronime(t *testing.T) {
-	result := scrape.Doronime("https://doronime.id/anime/mieruko-chan/episode-12")
-	writeToJson(result, "doronime")
-}
+func TestScrape(t *testing.T) {
+	tests := []struct {
+		name   string
+		link   string
+		scrape func(link string) scrape.ScrapeResponse
+	}{
+		{
+			name:   "doronime",
+			link:   "https://doronime.id/anime/mieruko-chan/episode-12",
+			scrape: scrape.Doronime,
+		},
+		{
+			name:   "lendrive",
+			link:   "https://lendrive.web.id/akebi-chan-no-sailor-fuku-ep-01-dual-subs-x265-hevc-subtitle-indonesia-english/",
+			scrape: scrape.Lendrive,
+		},
+		{
+			name:   "oploverz",
+			link:   "https://oploverz.co.in/danmachi-s4-episode-12-subtitle-indonesia/?utm_source=rss&utm_medium=rss&utm_campaign=danmachi-s4-episode-12-subtitle-indonesia",
+			scrape: scrape.Oploverz,
+		},
+		{
+			name:   "samehadaku",
+			link:   "https://194.163.183.129/saihate-no-paladin-episode-10/",
+			scrape: scrape.Samehadaku,
+		},
+		{
+			name:   "animekompi",
+			link:   "https://animekompi.net/hyouken-no-majutsushi-ga-sekai-wo-suberu-episode-01-subtitle-indonesia/",
+			scrape: scrape.Animekompi,
+		},
+	}
 
-func TestLendrive(t *testing.T) {
-	result := scrape.Lendrive("https://lendrive.web.id/akebi-chan-no-sailor-fuku-ep-01-dual-subs-x265-hevc-subtitle-indonesia-english/")
-	writeToJson(result, "lendrive")
-}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := test.scrape(test.link)
+			require.NotEmpty(t, result.Downloads)
+			writeToJson(result, test.name)
+		})
 
-func TestOploverz(t *testing.T) {
-	result := scrape.Oploverz("https://oploverz.co.in/danmachi-s4-episode-12-subtitle-indonesia/?utm_source=rss&utm_medium=rss&utm_campaign=danmachi-s4-episode-12-subtitle-indonesia")
-	writeToJson(result, "oploverz")
-}
+	}
 
-func TestSamehadaku(t *testing.T) {
-	result := scrape.Samehadaku("https://194.163.183.129/saihate-no-paladin-episode-10/")
-	writeToJson(result, "samehadaku")
-}
-
-func TestAnimeKompi(t *testing.T) {
-	result := scrape.Animekompi("https://animekompi.net/hyouken-no-majutsushi-ga-sekai-wo-suberu-episode-01-subtitle-indonesia/")
-	writeToJson(result, "animekompi")
 }
